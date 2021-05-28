6ca571f5b4d0a4541cc07ac8cdc273aacb82dc74
EthanTscode <eluca.tase@gmail.com>
2021-05-28 18:56:04 -0400

    FirstLessons

---
+++ Forloop.js
@@ -0,0 +1,14 @@
+// var index = Math.round(Math.random(4));
+// var Array1 = ['Hello', 'How are you?', 'String3', '^ The string above me is made out of cheese', 'Last string i guess'];
+
+// for(index; index < 5; index++){
+    // console.log(Array1[Math.floor((Math.random() * 5) + 0)]);
+// }
+
+// var index = Math.round(Math.random(4));
+// var Array1 = ['Hello', 'How are you?', 'String3', '^ The string above me is made out of cheese', 'Last string i guess'];
+
+// while(index < 5){
+  // console.log(Array1[Math.floor((Math.random() * 5) + 0)]);
+  // index++;
+// }
---
+++ test.js
@@ -0,0 +1,23 @@
+// Author: Ethan Tanase
+// Project: Random testing of everything learned summarized
+// Date: 5/25/2021
+// Updated: 5/26/2021
+
+// Loops I'm learning: For loops while loops and do while loops
+// Functions I'm learning: Normal functions and recursive functions
+// Variables I'm learning: ++score, score++, --score, score--, arrays (variable + [1, 2, etc])
+// Currently training in: Intermediate debugging
+// Will learn: Sorting algorithm (var[2, 3, 6, 4, 5]) --> (sorted 2, 3, 4, 5, 6)
+
+// Array = [Num1, num1, num3, etc]
+// For ([Start]; [Stop]; [Method]){
+
+// }
+// Print the index variable
+
+var index = 0;
+var Array1 = [0, 1, 2, 3, 4, 5];
+
+for(index; index < 5; index++){
+    print(index);
+}
